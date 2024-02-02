b0e56a4f63c6d4de9d81af53c7bbdc696b53bc3a
Kaiqueav <kaiquecerq66@outlook.com>
2024-02-02 15:21:44 -0300

    feat: create methods

--- src/pilha/Pilha.java
+++ src/pilha/Pilha.java
@@ -7,6 +7,22 @@ public class Pilha {
         this.refNoEntradaP = null;
     }
 
+    public void push(No newNo){
+        No refAuxiliar = refNoEntradaP;
+        refNoEntradaP = newNo;
+        refNoEntradaP.setRefNo(refAuxiliar);
+    }
+
+    public No pop(){
+        if(!this.isEmpty()){
+            No noPoped = refNoEntradaP;
+            refNoEntradaP = refNoEntradaP.getRefNo();
+            return noPoped;
+        }return null;
+    }
+    public No top(){
+        return refNoEntradaP;
+    }
     public boolean isEmpty(){
         return refNoEntradaP == null ? true : false;
     }
