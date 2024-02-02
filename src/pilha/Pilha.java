d2e8a587c06d66791aefaa50129fda7c9130d52c
Kaiqueav <kaiquecerq66@outlook.com>
2024-02-02 15:04:02 -0300

    FIRST COMMIT

---
+++ src/pilha/Pilha.java
@@ -0,0 +1,17 @@
+package pilha;
+
+public class Pilha {
+    private No refNoEntradaP;
+
+    public Pilha() {
+        this.refNoEntradaP = null;
+    }
+
+    public boolean isEmpty(){
+        return refNoEntradaP == null ? true : false;
+    }
+
+
+
+
+}
