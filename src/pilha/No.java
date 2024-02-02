cd51a82be38ccf9811fd11a7b17e9723c82e19df
Kaiqueav <kaiquecerq66@outlook.com>
2024-02-02 15:21:18 -0300

    fix: update bugs

--- src/pilha/No.java
+++ src/pilha/No.java
@@ -3,7 +3,7 @@ package pilha;
 public class No {
 
     private int dado;
-    private No reNo = null;
+    private No refNo = null;
 
 
     public No(){
@@ -20,12 +20,12 @@ public class No {
         this.dado = dado;
     }
 
-    public No getReNo() {
-        return reNo;
+    public No getRefNo() {
+        return refNo;
     }
 
-    public void setReNo(No reNo) {
-        this.reNo = reNo;
+    public void setRefNo(No reNo) {
+        this.refNo = reNo;
     }
 
     @Override
