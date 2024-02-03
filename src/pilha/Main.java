bbbe7b9e2fad90f23354fb7d89506b874aac6be0
Kaiqueav <kaiquecerq66@outlook.com>
2024-02-02 23:17:09 -0300

    feat: criando main

---
+++ src/pilha/Main.java
@@ -0,0 +1,16 @@
+package pilha;
+
+public class Main {
+    
+    public static void main(String[] args){
+        Pilha minhaPilha = new Pilha();
+
+        minhaPilha.push(new No(1));
+        minhaPilha.push(new No(2));
+        minhaPilha.push(new No(3));
+
+        System.out.println(minhaPilha);
+
+
+    }
+}
