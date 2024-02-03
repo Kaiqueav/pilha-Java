ed731c352b21ab1c191304a1d3e918982361cb6e
Kaiqueav <kaiquecerq66@outlook.com>
2024-02-02 23:50:47 -0300

    feat: testando pop e push

--- src/pilha/Main.java
+++ src/pilha/Main.java
@@ -11,6 +11,10 @@ public class Main {
 
         System.out.println(minhaPilha);
 
+        System.out.println(minhaPilha.pop());
+        System.out.println(minhaPilha);
 
+        minhaPilha.push(new No(99));
+        System.out.println(minhaPilha);
     }
 }
